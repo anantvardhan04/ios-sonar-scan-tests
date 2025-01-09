2e0f3fd44ae239439f905e38f545ec7520520859
Anant Vardhan <anantvardhan.04@gmail.com>
2025-01-09 17:42:35 +0530

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    func addNumbers(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
